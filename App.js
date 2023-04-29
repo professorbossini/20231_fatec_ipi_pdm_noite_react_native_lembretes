2c3599510ee211851e1e3c61f8a0b494ea1c3341
Rodrigo Bossini <professorbossini@gmail.com>
2023-04-28 22:16:44 -0300

    apostila05(lembretes): fim da secao 2.14

--- App.js
+++ App.js
@@ -2,21 +2,27 @@ import { useState } from 'react'
 import {
   Button,
   StyleSheet,
+  Text,
   TextInput, 
   View 
 } from 'react-native';
 
 export default function App() {
   const [lembrete, setLembrete] = useState('')
+  const [lembretes, setLembretes] = useState([])
   const capturarLembrete = (lembreteDigitado) => {
     setLembrete(lembreteDigitado)
   }
   const adicionarLembrete = () => {
-    console.log(lembrete)
+    setLembretes(lembretes => {
+      const aux = [lembrete, ...lembretes] //spread
+      setLembrete('')
+      return aux
+    })
   }
   return (
     <View style={styles.container}>
-      <View>
+      <View style={styles.lembreteView}>
         <TextInput 
           style={styles.lembreteTextInput}
           placeholder='Desejo lembrar...'
@@ -29,7 +35,12 @@ export default function App() {
         />
       </View>
       <View>
-
+          {/* [Comprar um abacate, ir ao cinema] => [<View><Text>Comprar um abacate</Text></View>, <View><Text>Ir ao cinema</Text></View>] */}
+          {
+            lembretes.map((l) => (
+              <View style={styles.itemNaLista}><Text>{l}</Text></View>
+            ))
+          }
       </View>
     </View>
   );
@@ -40,6 +51,9 @@ const styles = StyleSheet.create({
     padding: 40,
     width: '100%'
   },
+  lembreteView: {
+    marginBottom: 4
+  },
   lembreteTextInput: {
     borderBottomColor: '#CCC',
     borderBottomWidth: 1,
@@ -47,6 +61,15 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     outlineStyle: 'none',
     marginBottom: 4
+  },
+  itemNaLista: {
+    padding: 12,
+    borderColor: '#AAA',
+    borderWidth: 1,
+    backgroundColor: '#DDD',
+    marginBottom: 4,
+    textAlign: 'center',
+    borderRadius: 4  
   }
 })
 
